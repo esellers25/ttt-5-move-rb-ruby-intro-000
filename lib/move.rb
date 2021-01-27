006017e3229b945abc63fb2883ff75c93a8962c7
Erin Sellers <erin.e.sellers@gmail.com>
2021-01-27 17:23:32 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -6,7 +6,6 @@ def display_board(board)
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
-user_input = ["1"-"9"]
 def input_to_index(user_input)
-  user_input.to_i
+  user_input.to_i 
 end
